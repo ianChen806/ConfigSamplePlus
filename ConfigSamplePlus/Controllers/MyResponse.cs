666ea43c87437702a783444bbb88df633cf8c17f
Ian <ian.chen@taiwantaxi.com.tw>
2020-03-25 13:14:44 +0800

    add MyConfig.

---
+++ ConfigSamplePlus/Controllers/MyResponse.cs
@@ -0,0 +1,9 @@
+namespace ConfigSamplePlus.Controllers
+{
+    public class MyResponse
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ ConfigSamplePlus/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace ConfigSamplePlus.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IOptions<MyConfig> _config;
+
+        public WeatherForecastController(IOptions<MyConfig> config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public MyResponse Get()
+        {
+            return new MyResponse
+            {
+                Id = _config.Value.Id,
+                Name = _config.Value.Name
+            };
+        }
+    }
+}
